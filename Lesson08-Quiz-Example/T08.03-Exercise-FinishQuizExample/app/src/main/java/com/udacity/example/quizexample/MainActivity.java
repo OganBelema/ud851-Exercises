c9635e9fa1b9935e20bfca7db6c0927d57842c93
Belema Ogan <Oganbelema@gmail.com>
2019-05-31 20:14:08 +0100

    Display data on screen from cursor

--- Lesson08-Quiz-Example/T08.03-Exercise-FinishQuizExample/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
+++ Lesson08-Quiz-Example/T08.03-Exercise-FinishQuizExample/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
@@ -20,9 +20,11 @@ import android.content.ContentResolver;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Trace;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.udacity.example.droidtermsprovider.DroidTermsExampleContract;
 
@@ -48,6 +50,12 @@ public class MainActivity extends AppCompatActivity {
     // advance the app to the next word
     private final int STATE_SHOWN = 1;
 
+    private TextView mWordTextView;
+
+    private TextView mDefinitionTextView;
+    private int mWordColumn;
+    private int mDefinitionColumn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,8 +63,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Get the views
-        // TODO (1) You'll probably want more than just the Button
+        //  (1) You'll probably want more than just the Button
         mButton = (Button) findViewById(R.id.button_next);
+
+        mWordTextView = (TextView) findViewById(R.id.text_view_word);
+
+        mDefinitionTextView = (TextView) findViewById(R.id.text_view_definition);
 
         //Run the database operation to get the cursor off of the main thread
         new WordFetchTask().execute();
@@ -87,10 +99,25 @@ public class MainActivity extends AppCompatActivity {
         // Change button text
         mButton.setText(getString(R.string.show_definition));
 
-        // TODO (3) Go to the next word in the Cursor, show the next word and hide the definition
+        //  (3) Go to the next word in the Cursor, show the next word and hide the definition
         // Note that you shouldn't try to do this if the cursor hasn't been set yet.
         // If you reach the end of the list of words, you should start at the beginning again.
-        mCurrentState = STATE_HIDDEN;
+
+        if (mData != null){
+            if (!mData.moveToNext()){
+               mData.moveToFirst();
+            }
+
+            String word = mData.getString(mWordColumn);
+            mWordTextView.setText(word);
+
+            mDefinitionTextView.setVisibility(View.INVISIBLE);
+
+            String definition = mData.getString(mDefinitionColumn);
+            mDefinitionTextView.setText(definition);
+
+            mCurrentState = STATE_HIDDEN;
+        }
 
     }
 
@@ -99,15 +126,17 @@ public class MainActivity extends AppCompatActivity {
         // Change button text
         mButton.setText(getString(R.string.next_word));
 
-        // TODO (4) Show the definition
+        //  (4) Show the definition
         mCurrentState = STATE_SHOWN;
+        mDefinitionTextView.setVisibility(View.VISIBLE);
 
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // TODO (5) Remember to close your cursor!
+        //  (5) Remember to close your cursor!
+        mData.close();
     }
 
     // Use an async task to do the data fetch off of the main thread.
@@ -136,8 +165,13 @@ public class MainActivity extends AppCompatActivity {
             // Set the data for MainActivity
             mData = cursor;
 
-            // TODO (2) Initialize anything that you need the cursor for, such as setting up
+            //  (2) Initialize anything that you need the cursor for, such as setting up
             // the screen with the first word and setting any other instance variables
+
+            mWordColumn = mData.getColumnIndex(DroidTermsExampleContract.COLUMN_WORD);
+            mDefinitionColumn = mData.getColumnIndex(DroidTermsExampleContract.COLUMN_DEFINITION);
+
+            nextWord();
         }
     }
 
